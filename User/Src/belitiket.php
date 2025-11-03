c07d08aa3a685392b892812b4fc158ae832b8f21
Nico <nicolausnl05@gmail.com>
2025-11-03 19:08:44 +0700

    rapikan file

--- User/Src/belitiket.php
+++ User/Src/belitiket.php
@@ -1,16 +1,4 @@
 <?php
 include "../Connection/koneksi.php";
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fitur beli tiket</title>
-</head>
-<body>
-    <form action="" method="get"></form>
-</body>
-</html>
+$harga;
